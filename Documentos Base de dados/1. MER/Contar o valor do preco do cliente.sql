1184742c1158c419f7abf0c86243f4376ddfa736
Jaimepelembe <105989003+Jaimepelembe@users.noreply.github.com>
2022-11-10 19:12:45 +0200

    Fiz a proposta de desnormalizacao da BD
    Proposta de desnormalizacao da BD2

--- Documentos Base de dados/1. MER/Contar o valor do preco do cliente.sql
+++ Documentos Base de dados/1. MER/Contar o valor do preco do cliente.sql
@@ -4,5 +4,8 @@ SELECT * FROM veterinaria.cirurgia;
 
 SELECT * FROM veterinaria.cliente;
 SELECT * FROM veterinaria.animal;
+#Script original
+select historico_cirurgia.preco from historico_cirurgia,cliente,animal  where historico_cirurgia.idAnimal=animal.idAnimal and animal.idCliente=12 order by cliente.idCliente;
 
-select historico_cirurgia.preco from historico_cirurgia,cliente,animal  where historico_cirurgia.idAnimal=animal.idAnimal and animal.idCliente=11;
+#Script Adicional
+select historico_cirurgia.preco from historico_cirurgia join cliente join animal  where historico_cirurgia.idAnimal=animal.idAnimal and animal.idCliente=11;
